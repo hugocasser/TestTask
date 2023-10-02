08493a7eaa24fe25a7572b77b787d017fb20bfd8
Артём <hugochacasser@gmail.com>
2023-10-02 12:24:05 +0300

    initial commit

---
+++ TestTask/Controllers/UsersController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Controllers
+{
+    /// <summary>
+    /// Users controller.
+    /// DO NOT change anything here. Use created contract and provide only needed implementation.
+    /// </summary>
+    [Route("api/v1/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService userService;
+
+        public UsersController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        /// <summary>
+        /// Returns selected user. 
+        /// Selection rules are specified in Task description provided by recruiter
+        /// </summary>
+        [HttpGet]
+        [Route("selected-user")]
+        public async Task<IActionResult> Get()
+        {
+            var result = await this.userService.GetUser();
+            return this.Ok(result);
+        }
+
+        /// <summary>
+        /// Returns list of selected users. 
+        /// Selection rules are specified in Task description provided by recruiter
+        /// </summary>
+        [HttpGet]
+        [Route("selected-users")]
+        public async Task<IActionResult> GetUsers()
+        {
+            var result = await this.userService.GetUsers();
+            return this.Ok(result);
+        }
+    }
+}
